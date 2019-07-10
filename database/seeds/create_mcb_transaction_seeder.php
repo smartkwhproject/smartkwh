176792a9416ac246d1ae9b2889a66bc235f53380
Yuantoro Kamajaya <yuantkamajaya.sp@gmail.com>
2019-07-10 15:05:39 +0700

    Class Transaksi DOne

--- database/seeds/create_mcb_transaction_seeder.php
+++ database/seeds/create_mcb_transaction_seeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\TransaksiMcb;
 use Illuminate\Database\Seeder;
 
 class create_mcb_transaction_seeder extends Seeder
@@ -11,7 +12,7 @@ class create_mcb_transaction_seeder extends Seeder
      */
     public function run()
     {
-        for ($i = 0; $i < 44640; $i++) {
+        for ($i = 0; $i < 10; $i++) {
             $mcbTransaction = new TransaksiMcb();
             $dateTime       = date('Y-m-d H:i:s', strtotime("+ {$i} minute"));
             $payload        = [
@@ -40,63 +41,64 @@ class create_mcb_transaction_seeder extends Seeder
             ];
             $mcbTransaction->createData($payload);
         }
-        for ($i = 0; $i < 44640; $i++) {
-            $mcbTransaction = new TransaksiMcb();
-            $dateTime       = date('Y-m-d H:i:s', strtotime("+ {$i} minute"));
-            $payload        = [
-                'blok_id' => 2,
-                'tanggal' => date('Y-m-d', strtotime($dateTime)),
-                'waktu'   => date('H:i:s', strtotime($dateTime)),
-                'va'      => rand(220, 233),
-                'vb'      => rand(220, 233),
-                'vc'      => rand(220, 233),
-                'vab'     => rand(330, 402),
-                'vbc'     => rand(330, 402),
-                'vca'     => rand(330, 402),
-                'ia'      => rand(0, 2),
-                'ib'      => rand(0, 1),
-                'ic'      => rand(0, 2),
-                'pa'      => rand(0, 5),
-                'pb'      => rand(0, 5),
-                'pc'      => rand(0, 5),
-                'pt'      => rand(0, 5),
-                'pfa'     => rand(0, 5),
-                'pfb'     => rand(0, 5),
-                'pfc'     => rand(0, 5),
-                'ep'      => rand(0, 5),
-                'eq'      => rand(0, 3),
+        // for ($i = 0; $i < 44640; $i++) {
+        //     $mcbTransaction = new TransaksiMcb();
+        //     $dateTime       = date('Y-m-d H:i:s', strtotime("+ {$i} minute"));
+        //     $payload        = [
+        //         'blok_id' => 2,
+        //         'tanggal' => date('Y-m-d', strtotime($dateTime)),
+        //         'waktu'   => date('H:i:s', strtotime($dateTime)),
+        //         'va'      => rand(220, 233),
+        //         'vb'      => rand(220, 233),
+        //         'vc'      => rand(220, 233),
+        //         'vab'     => rand(330, 402),
+        //         'vbc'     => rand(330, 402),
+        //         'vca'     => rand(330, 402),
+        //         'ia'      => rand(0, 2),
+        //         'ib'      => rand(0, 1),
+        //         'ic'      => rand(0, 2),
+        //         'pa'      => rand(0, 5),
+        //         'pb'      => rand(0, 5),
+        //         'pc'      => rand(0, 5),
+        //         'pt'      => rand(0, 5),
+        //         'pfa'     => rand(0, 5),
+        //         'pfb'     => rand(0, 5),
+        //         'pfc'     => rand(0, 5),
+        //         'ep'      => rand(0, 5),
+        //         'eq'      => rand(0, 3),
 
-            ];
-            $mcbTransaction->createData($payload);
-        }
-        for ($i = 0; $i < 44640; $i++) {
-            $mcbTransaction = new TransaksiMcb();
-            $dateTime       = date('Y-m-d H:i:s', strtotime("+ {$i} minute"));
-            $payload        = [
-                'blok_id' => 3,
-                'tanggal' => date('Y-m-d', strtotime($dateTime)),
-                'waktu'   => date('H:i:s', strtotime($dateTime)),
-                'va'      => rand(220, 233),
-                'vb'      => rand(220, 233),
-                'vc'      => rand(220, 233),
-                'vab'     => rand(330, 402),
-                'vbc'     => rand(330, 402),
-                'vca'     => rand(330, 402),
-                'ia'      => rand(0, 2),
-                'ib'      => rand(0, 1),
-                'ic'      => rand(0, 2),
-                'pa'      => rand(0, 5),
-                'pb'      => rand(0, 5),
-                'pc'      => rand(0, 5),
-                'pt'      => rand(0, 5),
-                'pfa'     => rand(0, 5),
-                'pfb'     => rand(0, 5),
-                'pfc'     => rand(0, 5),
-                'ep'      => rand(0, 5),
-                'eq'      => rand(0, 3),
+        //     ];
+        //     $mcbTransaction->createData($payload);
+        // }
+        // for ($i = 0; $i < 44640; $i++)
+        // {
+        //     $mcbTransaction = new TransaksiMcb();
+        //     $dateTime       = date('Y-m-d H:i:s', strtotime("+ {$i} minute"));
+        //     $payload        = [
+        //         'blok_id' => 3,
+        //         'tanggal' => date('Y-m-d', strtotime($dateTime)),
+        //         'waktu'   => date('H:i:s', strtotime($dateTime)),
+        //         'va'      => rand(220, 233),
+        //         'vb'      => rand(220, 233),
+        //         'vc'      => rand(220, 233),
+        //         'vab'     => rand(330, 402),
+        //         'vbc'     => rand(330, 402),
+        //         'vca'     => rand(330, 402),
+        //         'ia'      => rand(0, 2),
+        //         'ib'      => rand(0, 1),
+        //         'ic'      => rand(0, 2),
+        //         'pa'      => rand(0, 5),
+        //         'pb'      => rand(0, 5),
+        //         'pc'      => rand(0, 5),
+        //         'pt'      => rand(0, 5),
+        //         'pfa'     => rand(0, 5),
+        //         'pfb'     => rand(0, 5),
+        //         'pfc'     => rand(0, 5),
+        //         'ep'      => rand(0, 5),
+        //         'eq'      => rand(0, 3),
 
-            ];
-            $mcbTransaction->createData($payload);
-        }
+        //     ];
+        //     $mcbTransaction->createData($payload);
+        // }
     }
 }
